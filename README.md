4b6051c4f785fe67892cc28a36889a7f77edf99b
Maximilian Stroh <Hisako1337@gmail.com>
2015-04-02 12:39:34 +0200

    initial publish

---
+++ README.md
@@ -0,0 +1,12 @@
+# jQqery.mmenu package
+
+This package wraps [mmenu](http://mmenu.frebsite.nl/) for meteor.
+
+## Installation
+
+`meteor add anonyfox:mmenu`
+
+## Documentation
+
+See http://mmenu.frebsite.nl/ for live examples and the source code
+on [github](https://github.com/BeSite/jQuery.mmenu).
--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   name: 'anonyfox:mmenu',
   version: '0.0.1',
   summary: 'Wrapper for http://mmenu.frebsite.nl/',
-  git: '',
+  git: 'https://github.com/Anonyfox/mmenu.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1');
-  api.use('mquandalle:bower@1.3.12_3')
+  api.use('mquandalle:bower@1.3.12_3');
   api.addFiles([
     "bower_components/jQuery.mmenu/dist/js/jquery.mmenu.min.all.js",
     "bower_components/jQuery.mmenu/dist/css/jquery.mmenu.all.css"
